886be74ae3241a61e302b8dc86ac5e33cae3e289
Qurashi Aqeel <qurashiakeel786@gmail.com>
2024-04-15 16:41:01 +0530

    add env, prettier config, editor config, code format.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,7 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+const fontSans = FontSans({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Pesto Todo",
